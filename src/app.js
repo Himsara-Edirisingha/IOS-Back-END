284a987d22f847b2d752a3eeeb35849156be314b
Himsara-Edirisingha <87166862+Himsara-Edirisingha@users.noreply.github.com>
2024-04-01 12:50:44 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const {
+  authenticateToken,
+} = require('./service/controllers/middleware/auth-controller.js');
+
+app.use(express.json());
+
+const categoriesRoute = require('./api/routes/categories.routes.js');
+const productsRoute = require('./api/routes/products.routes.js');
+const ordersoute = require('./api/routes/orders.routes.js');
+const paymentsRoute = require('./api/routes/payments.routes.js');
+const usersRoute = require('./api/routes/users.routes.js');
+const loginRoute = require('./api/routes/token.routes.js');
+const cartItemRoute = require('./api/routes/cartitem.routes.js');
+
+app.use('/api/categories', categoriesRoute);
+app.use('/api/products', productsRoute);
+app.use('/api/orders', ordersoute);
+app.use('/api/payments', paymentsRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/login', loginRoute);
+app.use('/api/carts', cartItemRoute);
+
+module.exports = app;
